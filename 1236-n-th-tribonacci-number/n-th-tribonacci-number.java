dcbb4780dd5ec608071ca5d64d1c5953096c9511
vivek jadhav <137620757+VivekJadhav07@users.noreply.github.com>
2026-07-22 20:11:44 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (71.92%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -12,16 +12,19 @@ class Solution {
         {
             return 1;
         }
-        int dp[]=new int[n+1];
-         dp[0]=0;
-         dp[1]=1;
-         dp[2]=1;
+     
+      int  prev3=0;
+     int   prev2=1;
+     int  prev1=1;
         for(int i=3;i<=n;i++)
         {
-            dp[i]=dp[i-3]+dp[i-2]+dp[i-1];
+         int curr=prev3+prev2+prev1;
+         prev3=prev2;
+         prev2=prev1;
+         prev1=curr;
           
         }
-        return dp[dp.length-1];
+        return prev1;
         
     }
 }
