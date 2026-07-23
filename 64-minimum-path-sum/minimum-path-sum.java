a46d8742a76d5033989e354670db52c9eb9945f9
vivek jadhav <137620757+VivekJadhav07@users.noreply.github.com>
2026-07-23 22:43:43 +0530

    Time: 6 ms (10.43%) | Memory: 50.4 MB (21.85%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -3,39 +3,52 @@ class Solution {
     public int minPathSum(int[][] grid) {
         
         int dp[][]=new int[grid.length][grid[0].length];
-        for(int i=0;i<dp.length;i++)
+        dp[0][0]=grid[0][0];
+        for(int i=0;i<grid.length;i++)
         {
-            Arrays.fill(dp[i],-1);
+              for(int j=0;j<grid[0].length;j++)
+                 {
+                    if(i==0 && j==0) continue;
+                    int left=Integer.MAX_VALUE;
+                    int down=Integer.MAX_VALUE;
+                  if(i>0) left=grid[i][j]+dp[i-1][j];
+                  if(j>0) down=grid[i][j]+dp[i][j-1];
+                  dp[i][j]=Math.min(left,down);
+                 }
+                 
+            
         }
+        return dp[dp.length-1][dp[0].length-1];
        
-       return f(grid,grid.length-1,grid[0].length-1,dp);
-    }
-    public int f(int [][] grid,int m,int n,int[][] dp)
-    {
-        
-        if(m==0 && n==0)
-        {
-           return grid[0][0];
-        }
-      
-
-        int left=Integer.MAX_VALUE;
-        int down=Integer.MAX_VALUE;
-        if(m>=0 && n>=0)
-        {
-         if(dp[m][n]!=-1) return dp[m][n];
-        }
-
-        if(n>0) 
-        {
-        left=grid[m][n]+ f(grid,m,n-1,dp);
-        }
-        if(m>0)
-        {
-         down=grid[m][n]+ f(grid,m-1,n,dp);
-        }
-        dp[m][n]= Math.min(left,down);
-          return dp[m][n];
-        
+    //    return f(grid,grid.length-1,grid[0].length-1,dp);
     }
 }
+//     public int f(int [][] grid,int m,int n,int[][] dp)
+//     {
+        
+//         if(m==0 && n==0)
+//         {
+//            return grid[0][0];
+//         }
+      
+
+//         int left=Integer.MAX_VALUE;
+//         int down=Integer.MAX_VALUE;
+//         if(m>=0 && n>=0)
+//         {
+//          if(dp[m][n]!=-1) return dp[m][n];
+//         }
+
+//         if(n>0) 
+//         {
+//         left=grid[m][n]+ f(grid,m,n-1,dp);
+//         }
+//         if(m>0)
+//         {
+//          down=grid[m][n]+ f(grid,m-1,n,dp);
+//         }
+//         dp[m][n]= Math.min(left,down);
+//           return dp[m][n];
+        
+//     }
+// }
