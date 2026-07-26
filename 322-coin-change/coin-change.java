fcd8eaa8abc78a510d197cc0799d2d3b393fc491
vivek jadhav <137620757+VivekJadhav07@users.noreply.github.com>
2026-07-26 18:51:31 +0530

    Time: 29 ms (21.51%) | Memory: 47.2 MB (16.13%) - LeetSync

---
+++ 322-coin-change/coin-change.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+       
+        int dp[][]=new int[coins.length][amount+1];
+        for(int []i:dp)
+        {
+            Arrays.fill(i,-1);
+        }
+        int ans=fun(coins.length-1,amount,coins,dp);
+        if( ans >=(int) 1e9){
+            return -1;
+        }
+        return ans;
+
+    }
+    public int fun(int i,int target,int[] coins,int[][] dp)
+    {
+         
+        if(target==0) return 0;
+        if(dp[i][target] != -1) return dp[i][target];
+        if(i==0)
+        {
+            if(target%coins[i]==0) return target/coins[0];
+            else return (int) 1e9;
+        }
+
+
+        int nottake=0+fun(i-1,target,coins,dp);
+        int take=Integer.MAX_VALUE;
+        if(coins[i]<=target)
+        {
+            take=1+fun(i,target-coins[i],coins,dp);
+        }
+        dp[i][target]= Math.min(take,nottake);
+        return dp[i][target];
+    }
+}
