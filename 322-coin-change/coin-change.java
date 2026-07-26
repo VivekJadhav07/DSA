5b86307af0d155c81d630b1ee48ae1aa2132b419
vivek jadhav <137620757+VivekJadhav07@users.noreply.github.com>
2026-07-26 19:10:44 +0530

    Time: 21 ms (35.15%) | Memory: 47.1 MB (24.87%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -2,36 +2,54 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
        
         int dp[][]=new int[coins.length][amount+1];
-        for(int []i:dp)
+       for(int t=0;t<=amount;t++)
+       {
+          if(t%coins[0]==0)
+          {
+            dp[0][t]= t/coins[0];
+          }else{
+            dp[0][t]=(int)1e9;
+          }
+
+       }
+       for(int i=1;i<coins.length;i++)
+       {
+        for(int t=0;t<=amount;t++)
         {
-            Arrays.fill(i,-1);
+             int nottake=0+dp[i-1][t];
+  int take=Integer.MAX_VALUE;
+      if(coins[i]<=t)
+      {
+         take=1+dp[i][t-coins[i]];
+      }
+     dp[i][t]= Math.min(take,nottake);
+    
+
         }
-        int ans=fun(coins.length-1,amount,coins,dp);
-        if( ans >=(int) 1e9){
-            return -1;
-        }
-        return ans;
+       }
+       if(dp[coins.length-1][amount]>=(int) 1e9) return -1;
+       return dp[coins.length-1][amount];
 
     }
-    public int fun(int i,int target,int[] coins,int[][] dp)
-    {
+    // public int fun(int i,int target,int[] coins,int[][] dp)
+    // {
          
-        if(target==0) return 0;
-        if(dp[i][target] != -1) return dp[i][target];
-        if(i==0)
-        {
-            if(target%coins[i]==0) return target/coins[0];
-            else return (int) 1e9;
-        }
+    //     if(target==0) return 0;
+    //     if(dp[i][target] != -1) return dp[i][target];
+    //     if(i==0)
+    //     {
+    //         if(target%coins[i]==0) return target/coins[0];
+    //         else return (int) 1e9;
+    //     }
 
 
-        int nottake=0+fun(i-1,target,coins,dp);
-        int take=Integer.MAX_VALUE;
-        if(coins[i]<=target)
-        {
-            take=1+fun(i,target-coins[i],coins,dp);
-        }
-        dp[i][target]= Math.min(take,nottake);
-        return dp[i][target];
-    }
+    //     int nottake=0+fun(i-1,target,coins,dp);
+    //     int take=Integer.MAX_VALUE;
+    //     if(coins[i]<=target)
+    //     {
+    //         take=1+fun(i,target-coins[i],coins,dp);
+    //     }
+    //     dp[i][target]= Math.min(take,nottake);
+    //     return dp[i][target];
+    // }
 }
