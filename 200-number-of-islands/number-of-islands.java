84f9337a44604b83176063ad30bd93dfa3c84428
vivek jadhav <137620757+VivekJadhav07@users.noreply.github.com>
2026-08-01 20:14:00 +0530

    Time: 6 ms (20.27%) | Memory: 51.6 MB (83.69%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,54 +1,51 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int row=grid.length;
-        int col=grid[0].length;
-        boolean[][] visited=new boolean[row][col];
+        int n=grid.length;
+        int m=grid[0].length;
+
+        boolean visited[][]=new boolean[n][m];
         Deque<int[]> queue=new ArrayDeque<>();
         int count=0;
-        for(int i=0;i<row;i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<col;j++)
+            for(int j=0;j<m;j++)
             {
-                if(visited[i][j]==false && grid[i][j]=='1')
-                {
+                 if(visited[i][j]==false && grid[i][j]=='1')
+                 {
+                    bfs(i,j,queue,visited,grid);
                     count+=1;
-                    bfs(i,j,grid,visited,queue);
-                }
+                 }
+
             }
         }
         return count;
-
     }
-
-    public void bfs(int row,int col, char [][]grid,boolean[][] visited,Deque<int[]> queue)
+    public void bfs(int i,int j,Deque<int[]> queue,boolean[][] visited,char[][] grid)
     {
-        int[] dr={-1,0,1,0};
-        int[] dc={0,1,0,-1};
-      int n=grid.length;
-      int m=grid[0].length;
-        queue.offer(new int[]{row,col});
-        visited[row][col]=true;
-       while(!queue.isEmpty())
-       {
-           int[] curr=queue.poll();
-           int r=curr[0];
-           int c=curr[1];
-
-           for(int k=0;k<4;k++)
-           {
-            int rc=r+dr[k];
-            int cc=c+dc[k];
-            if(rc>=0 && rc<n && cc>=0 && cc<m && visited[rc][cc]==false && grid[rc][cc]=='1')
+        int dr[]={-1,0,1,0};
+        int dc[]={0,1,0,-1};
+        int row=grid.length;
+        int col=grid[0].length;
+        queue.offer(new int[]{i,j});
+        while(!queue.isEmpty())
+        {
+         int[] curr=queue.poll();
+         int r=curr[0];
+         int c=curr[1];
+            
+            for(int k=0;k<4;k++)
             {
-                visited[rc][cc]=true;
-                queue.offer(new int[]{rc,cc});
+                int rc=r+dr[k];
+                int cc=c+dc[k];
+                if(rc>=0 && rc<row && cc>=0 && cc<col && visited[rc][cc]==false && grid[rc][cc]=='1')
+
+                {
+                    visited[rc][cc]=true;
+                    queue.offer(new int[]{rc,cc});
+                }
             }
 
-           }
-
-       }
-
-
+        }
 
 
     }
