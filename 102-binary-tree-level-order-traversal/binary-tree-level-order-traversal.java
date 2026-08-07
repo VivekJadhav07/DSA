f12d2b1a0e46e163110a4f210fb64c461cc45a89
vivek jadhav <137620757+VivekJadhav07@users.noreply.github.com>
2026-08-07 07:26:57 +0530

    Time: 1 ms (95.86%) | Memory: 47.1 MB (7.81%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -15,40 +15,37 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-       
-       List<List<Integer>> list=new ArrayList<>();
-       Deque<TreeNode> queue=new ArrayDeque<>();
- if(root==null)
+        List<List<Integer>> res=new ArrayList<>();
+         if(root==null)
+         {
+            return res;
+         }
+        Deque<TreeNode> deque=new ArrayDeque<>();
+        deque.offer(root);
+        while(!deque.isEmpty())
         {
-            return list;
-        }
-       queue.offer(root);
-    //     ArrayList<Integer> list3=new ArrayList<>();
-    //     list3.add(root.val);
-    //    list.add(list3);
-       while(!queue.isEmpty())
-       {
-         ArrayList<Integer> list1=new ArrayList<>();
-           int k=queue.size();
-          for(int i=0;i<k;i++)
-          {
-          TreeNode temp=queue.poll();
-           list1.add(temp.val);
-          if(temp.left!=null)
-          {
-          queue.offer(temp.left);
-          }
-          if(temp.right!=null)
-          {
-            queue.offer(temp.right);
-          }
-          }
-         
-        
-          list.add(list1);
+           ArrayList<Integer> list=new ArrayList<>();
+           int k=deque.size();
+           for(int i=0;i<k;i++)
+           {
+              TreeNode ele=deque.poll();
+              list.add(ele.val);
+              if(ele.left!=null)
+              {
+                deque.add(ele.left);
+              }
+                if(ele.right!=null)
+              {
+                deque.add(ele.right);
+              }
+           }
 
-          
-       }
-        return list;
+           res.add(list);
+
+
+
+
+        }
+        return res;
     }
 }
