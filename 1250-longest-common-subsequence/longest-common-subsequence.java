15b728857d4414a672c6749157dcf32cb3b0126b
vivek jadhav <137620757+VivekJadhav07@users.noreply.github.com>
2026-08-08 14:45:15 +0530

    Time: 25 ms (29.39%) | Memory: 53.9 MB (70.13%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,59 +1,34 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int dp[][]=new int[text1.length()+1][text2.length()+1];
-        for(int i=0;i<=text1.length();i++)
-        {
-            dp[i][0]=0;
-        }
-            for(int i=0;i<=text2.length();i++)
-        {
-            dp[0][i]=0;
-        }
-    //     for(int []i:dp)
-    //     {
-    //         Arrays.fill(i,-1);
-    //     }
-    //   int i1=text1.length()-1;
-    //   int i2=text2.length()-1;
-    //     return fun(i1,i2,text1,text2,dp);
-    for(int i1=1;i1<=text1.length();i1++)
+        int dp[][]=new int[text1.length()][text2.length()];
+       for(int []i:dp)
+       {
+        Arrays.fill(i,-1);
+       }
+        return fun(text1.length()-1,text2.length()-1,text1,text2,dp);
+        
+    }
+    public  int fun(int i,int j,String text1,String text2,int[][] dp)
     {
-        for(int i2=1;i2<=text2.length();i2++)
-        {
-        int s1=text1.charAt(i1-1);
-        int s2=text2.charAt(i2-1);
-        if(s1==s2) {
-        dp[i1][i2]= 1+dp[i1-1][i2-1];
-        }else{
+           if(i<0 ||j<0) return 0;
+           if(dp[i][j]!=-1) return dp[i][j];
+           
+            char ch1=text1.charAt(i);
+            char ch2=text2.charAt(j);
+           if(ch1==ch2)
+           {
+              return 1+fun(i-1,j-1,text1,text2,dp);
+           }
+            
+            
+             dp[i][j]= Math.max(  fun(i-1,j,text1,text2,dp),fun(i,j-1,text1,text2,dp));
+             return dp[i][j];
 
-           dp[i1][i2] = Math.max(dp[i1-1][i2],dp[i1][i2-1]);
+           
+         
+           
 
-        }
-        
-
-        }
-        
+            
+           
     }
-    return dp[text1.length()][text2.length()];
-        
-    }
-//     public int fun(int i1,int i2,String text1,String text2,int[][] dp)
-//     {
-//         if(i1<0 || i2<0) return 0;
-//         if(dp[i1][i2]!=-1) return dp[i1][i2];
-//         int s1=text1.charAt(i1);
-//         int s2=text2.charAt(i2);
-//         if(s1==s2) {
-//         dp[i1][i2]= 1+fun(i1-1,i2-1,text1,text2,dp);
-//         }else{
-
-//            dp[i1][i2] = Math.max(fun(i1-1,i2,text1,text2,dp),fun(i1,i2-1,text1,text2,dp));
-
-//         }
-//         return dp[i1][i2];
-
-
-
-//     }
-// }
 }
