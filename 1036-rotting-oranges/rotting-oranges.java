b4734d7dc23ccfc6c2b180c1c008d1624c970f4b
vivek jadhav <137620757+VivekJadhav07@users.noreply.github.com>
2026-08-08 08:20:36 +0530

    Time: 1 ms (100.00%) | Memory: 44.2 MB (40.42%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,73 +1,91 @@
 class Solution {
-   
     public int orangesRotting(int[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
-        Deque< int[]> queue=new ArrayDeque<>();
+        int m=grid.length;
+        int n=grid[0].length;
         int fresh=0;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<m;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                   fresh+=1; 
+                }
+            }
+        }
+       
+        Deque< int[]> deque=new ArrayDeque<>();
+       
+             for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
             {
                 if(grid[i][j]==2)
                 {
-                  queue.offer(new int[]{i,j});
-                 
-                   
-                }else if(grid[i][j]==1)
-                {
-                    fresh++;
-
+                   deque.offer(new int[]{i,j});
                 }
             }
         }
-        if(fresh==0)
+        int []dr={-1,0,1,0};
+        int []dc={0,1,0,-1};
+         int time=0;
+
+        while(!deque.isEmpty())
         {
-            return 0;
-        }
-        int dr[]={-1,0,1,0};
-        int dc[]={0,1,0,-1};
-        int time=0;
-    while( !queue.isEmpty() )
-    {
-        int size=queue.size();
-        boolean rotten=false;
-        for(int i=0;i<size;i++)
-        {
-            int curr[]=queue.poll();
-            int row=curr[0];
-            int col=curr[1];
-            for(int k=0;k<4;k++)
+            int size=deque.size();
+            boolean rotten=false;
+            for(int k=0;k<size;k++)
             {
-                int rc=row+dr[k];
-                int cc=col+dc[k];
-                if(rc>=0 && rc<n && cc>=0 && cc<m && grid[rc][cc]==1)
-                {
-                    grid[rc][cc]=2;
-                    rotten=true;
-                    fresh--;
-                    queue.offer(new int[]{rc,cc});
-                }
-
-            }
-
-        }
-        if(rotten)
-        {
-            time+=1;
-        }
-    }
-    
-    if(fresh==0)
-    {
-        return time;
-    }else{
-        return -1;
-    }
-
+         int[] curr=deque.poll();
+         int row=curr[0];
+         int col=curr[1];
          
+         
+           for(int i=0;i<4;i++)
+           {
+              int rc=row+dr[i];
+              int cc=col+dc[i];
+              if(rc>=0 && rc<m && cc>=0 && cc<n &&
+              grid[rc][cc]==1 
+              )
+              {
+                rotten=true;
+               deque.offer(new int[]{rc,cc});
+                grid[rc][cc]=2;
+              }
+
+
+           }
+            }
+           if(rotten==true)
+           {
+            time+=1;
+           }
+           
+         
+        }
+        int count=0;
+                for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                   return -1;
+                }
+            }
+        }
+       
+      
+
+           return time;
         
     }
 
-    
+    // public int bfs(fresh,grid,deque,visited)
+    // {
+    //     int m=grid.length;
+    //     int n=grid[0].length;
+        
+    // }
 }
