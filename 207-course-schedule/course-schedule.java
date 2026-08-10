53fe49ae51ee3c48668f9125202904077850b12d
vivek jadhav <137620757+VivekJadhav07@users.noreply.github.com>
2026-08-10 11:48:56 +0530

    Time: 7 ms (56.10%) | Memory: 47.5 MB (12.92%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] edges) {
+        ArrayList<ArrayList<Integer>> list=new ArrayList<>();
+        for(int i=0;i<numCourses;i++)
+        {
+            list.add(new ArrayList<Integer>());
+        }
+        for(int i=0;i<edges.length;i++)
+        {
+            int u=edges[i][0];
+            int v=edges[i][1];
+            list.get(v).add(u);
+        }
+        int indegree[] =new int[numCourses];
+        for(int i=0;i<numCourses;i++)
+        {
+            for(int node:list.get(i))
+            {
+                indegree[node]++;
+            }
+        }
+        Deque<Integer> deque=new ArrayDeque<>();
+        ArrayList<Integer> res=new ArrayList<>();
+          for(int i=0;i<numCourses;i++)
+        {
+           if(indegree[i]==0)
+           {
+            deque.offer(i);
+           }
+        }
+
+        while(!deque.isEmpty())
+        {
+            int node=deque.poll();
+            res.add(node);
+            for(int i:list.get(node))
+            {
+                indegree[i]--;
+                if(indegree[i]==0)
+                {
+                    deque.offer(i);
+                }
+            }
+        }
+       if(res.size()<numCourses)
+       {
+        return false;
+       }
+       return true;
+
+        
+    }
+}
