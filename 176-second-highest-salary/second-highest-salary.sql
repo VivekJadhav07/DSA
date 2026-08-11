62ddf59f39ee3568894f79c50ce71bd61bfdf20a
vivek jadhav <137620757+VivekJadhav07@users.noreply.github.com>
2026-08-11 14:35:34 +0530

    Time: 304 ms (41.48%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,13 +1,5 @@
 # Write your MySQL query statement below
-select max(salary) as SecondHighestSalary from(
-
-select id,salary  ,
-dense_rank()
-over(
-
-    order by salary desc
-) as sal
+select max(salary) as SecondHighestSalary
 from Employee
-
-) as t
- where sal=2;
+where salary <
+( select max(salary) from Employee)
