575f5de6e428743f2e69813d315d8ea572649787
vivek jadhav <137620757+VivekJadhav07@users.noreply.github.com>
2026-08-12 14:41:15 +0530

    Time: 49 ms (50.83%) | Memory: 47.6 MB (53.60%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,40 +1,64 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-      
-        int sum=0;
-        for(int i:nums)
-        {
-            sum+=i;
-        }
-     if(sum%2!=0) return false;
-      int target = sum / 2;
-       int dp[][]=new int[nums.length][target+1];
-       for(int i[]:dp)
+         int sum=0;
+       for(int i:nums)
        {
-        Arrays.fill(i,-1);
+        sum+=i;
        }
+       if(sum%2!=0) return false;
+       int target=sum/2;
+       
+
+       boolean dp[][]=new boolean[nums.length][target+1];
       
-        return fun(nums.length-1,target,nums,dp);
+        for(int i=0;i<nums.length;i++)
+        {
+            dp[i][0]=true;
+        }
+          if(nums[0]<=target)
+        {
+            dp[0][nums[0]]=true;
+        }
+
+         for(int i=1;i<nums.length;i++)
+         {
+            for(int k=1;k<=target;k++)
+            {
+                 boolean nottake=dp[i-1][k];
+        boolean take=false;
+         if(nums[i]<=k)
+         {
+            take=dp[i-1][k-nums[i]];
+         }
+        
+         dp[i][k]= take||nottake;
+
+            }
+         }
+         return dp[dp.length-1][target];
+
+
+
         
     }
-    public boolean fun(int i,int target,int[] nums,int[][] dp)
-    {
-        if(target==0) return true;
-        if(i==0) return nums[0]==target;
-        if(dp[i][target]!=-1) return dp[i][target]==1;
+    // public boolean fun(int i,int target,int[] nums,int[][] dp)
+    // {
+    //     if(target==0) return true;
+    //     if(i==0) return nums[0]==target;
+    //     if(dp[i][target]!=-1) return dp[i][target]==1;
 
-        boolean nottake=fun(i-1,target,nums,dp);
-        boolean take=false;
-         if(nums[i]<=target)
-         {
-            take=fun(i-1,target-nums[i],nums,dp);
-         }
-         if( take||nottake)
-         {
-            dp[i][target]=1;
-         }else{
-              dp[i][target]=0;
-         }
-         return take||nottake;
-    }
+    //     boolean nottake=fun(i-1,target,nums,dp);
+    //     boolean take=false;
+    //      if(nums[i]<=target)
+    //      {
+    //         take=fun(i-1,target-nums[i],nums,dp);
+    //      }
+    //      if( take||nottake)
+    //      {
+    //         dp[i][target]=1;
+    //      }else{
+    //           dp[i][target]=0;
+    //      }
+    //      return take||nottake;
+    // }
 }
