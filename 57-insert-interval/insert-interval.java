db6da158d31761131d59c4dd0606846e7419b857
vivek jadhav <137620757+VivekJadhav07@users.noreply.github.com>
2026-08-19 15:54:04 +0530

    Time: 1 ms (98.14%) | Memory: 47.3 MB (7.25%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,33 +1,35 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         ArrayList<int[]> list=new ArrayList<>();
-        int k=0;
+         int k=0;
         while(k<intervals.length && newInterval[0]>intervals[k][1])
         {
-           list.add(intervals[k]);
-           k+=1;
+            list.add(intervals[k]);
+            k+=1;
+            
         }
+
           while(k<intervals.length && newInterval[1]>=intervals[k][0])
         {
-           newInterval[0]=Math.min(intervals[k][0],newInterval[0]);
-           newInterval[1]=Math.max(intervals[k][1],newInterval[1]);
-           k+=1;
+           newInterval[0]=Math.min(newInterval[0],intervals[k][0]);
+            newInterval[1]=Math.max(newInterval[1],intervals[k][1]);
+            k+=1;
         }
         list.add(new int[]{newInterval[0],newInterval[1]});
-         while(k<intervals.length)
+           while(k<intervals.length )
         {
-           list.add(intervals[k]);
-           k+=1;
+            list.add(intervals[k]);
+            k+=1;
         }
-        int res[][]=new int[list.size()][2];
-       for(int i=0;i<list.size();i++)
-       {
-         res[i]=list.get(i);  
+        int arr[][]=new int[list.size()][2];
+        for(int i=0;i<list.size();i++)
+        {
+            int temp[]=list.get(i);
+            arr[i][0]=temp[0];
+            arr[i][1]=temp[1];
 
-       }
-       return res;
-       
+        }
+        return arr;
 
     }
-
 }
