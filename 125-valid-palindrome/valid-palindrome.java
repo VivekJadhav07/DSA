5dec47881e37aa9e20364930a2aa7a2e03ef9116
vivek jadhav <137620757+VivekJadhav07@users.noreply.github.com>
2026-08-23 18:34:10 +0530

    Time: 129 ms (5.00%) | Memory: 47.8 MB (8.68%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,17 +1,28 @@
 class Solution {
     public boolean isPalindrome(String s) {
 
-       String s1=s.toLowerCase(); 
-       String s2="";
+       String s1=s.toLowerCase();
+       String str="";
        for(char ch:s1.toCharArray())
        {
-          if ( (ch>='a' && ch<='z') || (ch>='0' && ch<='9'))
-          {
-           s2+=ch;
-          }
+        if(( ch>='a' && ch<='z' ) || (ch>='0' && ch<='9'))
+        {
+            str+=ch;
+        }
        }
-       StringBuilder sb=new StringBuilder(s2);
-       String s3=sb.reverse().toString();
-       return s2.equals(s3);
+      int left=0;
+      int right=str.length()-1;
+      while(left<right)
+      {
+        if(str.charAt(left)!=str.charAt(right))
+        {
+            return false;
+        }
+        left+=1;
+        right-=1;
+
+      }
+      return true;
+
     }
 }
