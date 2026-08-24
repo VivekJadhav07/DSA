70d01a97f88415ccc185a9c08b645334d2cbed1e
vivek jadhav <137620757+VivekJadhav07@users.noreply.github.com>
2026-08-24 17:44:18 +0530

    Time: 5 ms (14.88%) | Memory: 45.7 MB (10.83%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,41 +1,35 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
         HashMap<Integer,Integer> map=new HashMap<>();
-Deque<Integer> stack=new ArrayDeque<Integer>();
+        Deque<Integer> stack=new ArrayDeque<>();
+        ArrayList<Integer> list=new ArrayList<Integer>();
         for(int i=nums2.length-1;i>=0;i--)
         {
-               while(!stack.isEmpty() && stack.peek()<nums2[i])
-               {
-                stack.pop();
-               }
-               if(stack.isEmpty())
-               {
-               map.put(nums2[i],-1);
-               
-
-               }else{
-                if(stack.peek()>nums2[i])
-                {
-                   
-                    map.put(nums2[i],stack.peek());
-                  
-                }
-               }
-                  stack.push(nums2[i]);
-              
-
-            
-
-        
-        }
-
-        for(int k=0;k<nums1.length;k++)
-        {
-            if(map.containsKey(nums1[k]))
+            while( !stack.isEmpty() && stack.peek()<nums2[i]  )
             {
-            nums1[k]=map.get(nums1[k]);
+                stack.pop();
             }
+            if(stack.isEmpty())
+            {
+                map.put(nums2[i],-1);
+            }else if(stack.peek()>nums2[i])
+            {
+                map.put(nums2[i],stack.peek());
+            }
+            stack.push(nums2[i]);
         }
-       return nums1; 
+        for(int i:nums1)
+        {
+          list.add(map.get(i));
+        }
+       int arr[]=new int[nums1.length];
+       for(int i=0;i<arr.length;i++)
+       {
+         arr[i]=list.get(i);
+       }
+
+       return arr;
+        
     }
 }
