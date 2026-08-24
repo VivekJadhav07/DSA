b45d40ae194f5411101daa7cf22611f9f32bdff6
vivek jadhav <137620757+VivekJadhav07@users.noreply.github.com>
2026-08-24 09:11:48 +0530

    Time: 70 ms (5.86%) | Memory: 63.5 MB (70.35%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,49 +1,39 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
      
-     int dp[][]=new int[s.length()][s.length()];
-     for(int i[]:dp)
-     {
-        Arrays.fill(i,-1);
-     }
+     int dp[][]=new int[s.length()+1][s.length()+1];
+    
+    for(int i=0;i<=s.length();i++)
+    {
+        dp[0][i]=0;
+        dp[i][0]=0;
+
+    }
      String s1=new StringBuilder(s).reverse().toString();
 
 
+        for(int i=1;i<=s.length();i++)
+        {
+            for(int j=1;j<=s1.length();j++)
+            {
+               char ch1=s.charAt(i-1);
+               char ch2=s1.charAt(j-1);
+               if(ch1==ch2)
+               {
+                dp[i][j]=1+dp[i-1][j-1];
+               }else{
+               int left=dp[i-1][j];
+               int right=dp[i][j-1];
+               dp[i][j]=Math.max(left,right);
+               }
+            }
+        }
 
-
-      return fun(s.length()-1,s1.length()-1,s,s1,dp);
+      return dp[dp.length-1][dp[0].length-1];
      
         
     }
-    public int fun(int i,int j,String s1,String s2,int[][] dp)
-    {
-       if(i<0 || j<0) return 0;
-      if(i==0 || j==0 )
-      {
-        if(s1.charAt(i)==s2.charAt(j))
-        {
-            return 1;
-        }
-      }
-      if(dp[i][j]!=-1) return dp[i][j];
-
- 
-      char ch1=s1.charAt(i);
-      char ch2=s2.charAt(j);
-      if(ch1==ch2)
-      {
-        dp[i][j]= 1+fun(i-1,j-1,s1,s2,dp);
-        return dp[i][j];
-      }
-      int left=fun(i-1,j,s1,s2,dp);
-      int right=fun(i,j-1,s1,s2,dp);
-      dp[i][j]= Math.max(left,right);
-      return dp[i][j];
-
-
-
-
-    }
+    
 
 
 }
