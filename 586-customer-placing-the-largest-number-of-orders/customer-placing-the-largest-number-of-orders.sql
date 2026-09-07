fd5c7017786e9b142c0bb8af73b9a41d31acd8c7
vivek jadhav <137620757+VivekJadhav07@users.noreply.github.com>
2026-09-07 10:21:17 +0530

    Time: 458 ms (85.60%) | Memory: 0B (100.00%) - LeetSync

--- 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -1,7 +1,10 @@
 # Write your MySQL query statement below
 
+
+
 select customer_number
-from Orders 
-GROUP BY customer_number
-order by count(customer_number)   desc
+from Orders
+group by customer_number
+order by count(customer_number) desc
 limit 1
+
